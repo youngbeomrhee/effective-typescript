de6df63202ae0001180932ae90db2f167d615a51
youngbeomrhee <youngbeomrhee@gmail.com>
2024-02-29 00:02:10 +0900

    feat: eslint no-inferrable-types 추가

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,4 +4,5 @@ module.exports = {
     parser: '@typescript-eslint/parser',
     plugins: ['@typescript-eslint'],
     root: true,
-}
+    rules: { '@typescript-eslint/no-inferrable-types': 'error' },
+};
